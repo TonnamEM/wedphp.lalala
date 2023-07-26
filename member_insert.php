3b681882c4f7d5cd89def6e3317335e73617c200
TonnamEM <137257414+TonnamEM@users.noreply.github.com>
2023-07-26 20:52:18 +0700

    Add files via upload

---
+++ member_insert.php
@@ -0,0 +1,29 @@
+<div class="container" >
+
+<?php
+include "connect.php";
+
+$filename = $_FILES["photo"]["name"];
+$tempname = $_FILES["photo"]["tmp_name"];
+$folder = "./images/" . $filename;
+
+$sql = "INSERT INTO  tbl_member (firstname, lastname, email, password, phone, photo, address)
+VALUES ('$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[password]','$_POST[phone]','$filename','$_POST[address]')";
+
+if (mysqli_query($conn, $sql)) {
+  
+  if (move_uploaded_file($tempname, $folder)) {
+    echo "อับโหลดรู้ได้แล้ว";
+  } else {
+    echo "อับโหลดรูปไม่ได้";
+  }
+
+  echo "New record created successfully";
+  header("location: member_show.php" );
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+?>
+</div>
---
+++ member_show.php
@@ -0,0 +1,47 @@
+<link  href="styles.css" rel="stylesheet" />
+<div class="center" >
+<?php
+    include "connect.php";
+
+    $sql = "SELECT * FROM tbl_member";
+    $result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+?>
+<table class="table">
+  <th> ชื่อ - นามสกุล </th>
+  <th> อีเมล </th>
+  <th> เบอร์โทร </th>
+  <th> ที่อยู่ </th>
+  <th> รูป  </th>
+  <th> Edit </th>
+  <th> Delete </th>
+<?php
+  while($row = mysqli_fetch_assoc($result)) {
+?>    
+  <tr>
+        <td><?php echo "$row[firstname] $row[lastname]"; ?> </td>
+        <td><?php echo "$row[email]"; ?> </td>
+        <td><?php echo "$row[phone]"; ?> </td>
+        <td><?php echo "$row[address]"; ?> </td>
+        <td><img src="./images/<?php echo $row[photo] ?>" width="200px" />  </td>
+        <td><?php echo"<a href='member_edit.php?id=$row[id]'> Edit </a> "; ?> </td>
+        <td><?php echo"<a href='member_del.php?id=$row[id]'> Delete </a> "; ?> </td>
+  </tr>
+
+<?php    
+    }
+?>
+
+</table>
+
+<?php
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+
+?>
+</div>
